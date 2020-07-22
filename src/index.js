b482c54e18c33c3c368e56e51a29b90ada09c5c0
R A Van Epps <rave@ravanepps.com>
2020-07-22 17:00:02 -0600

    Hide original roll buttons

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import './scss/style.scss';
+
+/**
+ * Extend Dice Roller to generate character stats.
+ */
+
+const blockClass = '.wp-block-group.character-generator';
+const blocks = document.querySelectorAll( blockClass );
+
+/**
+ * Customize character generator blocks.
+ *
+ * @author R A Van Epps <rave@ravanepps.com>
+ * @since  1.0.0
+ *
+ * @param  {Object} Block DOM element.
+ */
+blocks.forEach( ( block ) => {
+	const buttons = block.querySelectorAll( 'button.roll-dice' );
+
+	// Hide original buttons.
+	if ( buttons.length > 0 ) {
+		buttons.forEach( ( button ) => {
+			button.style.display = 'none';
+		} );
+	}
+} );
